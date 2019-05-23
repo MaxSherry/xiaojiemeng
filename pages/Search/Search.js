ad49fde6c61b0c2012b3ef56ed6256e44d565710
xinsheng <xinsheng@chaoxing.com>
2019-05-23 17:04:45 +0800

    风琴缺少函数问题修复

--- pages/Search/Search.js
+++ pages/Search/Search.js
@@ -46,10 +46,10 @@ Page({
 				//delta:1
 				url: '../SearchTip/SearchTip'
 			})
-		}else{
+		} else {
 			this.data.isCancelTap = false
 			wx.navigateBack({
-				delta:2
+				delta: 2
 			})
 		}
 	},
@@ -59,5 +59,11 @@ Page({
 			delta:2
 			// url: '../JieMengList/JieMengList'
 		}) */
+	},
+	onSearch(event) {
+		this.setData({
+			activeNames: event.detail,
+		});
+
 	}
 })
